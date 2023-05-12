e227ac037df4c0ba066449a14e3ab2d54b931e49
phuoctai0612 <sieuem2002@gmail.com>
2023-05-12 11:16:28 +0700

    baitap1

---
+++ bai_tap/bai_tap_1.sql
@@ -0,0 +1,20 @@
+create database ss1 ;
+use ss1;
+create table table1 (
+      id int primary key auto_increment ,
+      nameStudent varchar(45)
+);
+insert into table1(nameStudent)
+value ("Tai"),("Kiet");
+
+select * from table1;
+alter table table1 rename to Student;
+create table Teacher (
+id int primary key auto_increment,
+nameTeacher varchar(45),
+age int,
+country varchar(45)
+);
+insert into teacher(nameTeacher,age,country)
+value("ChanhTv",35,"DaNang");
+select * from Teacher
